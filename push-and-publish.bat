5349ca727202dfaf5e04422cd1fa85c921f941c9
Guillaume Aldebert <guillaume.aldebert@autodesk.com>
2018-01-23 14:05:37 +0100

    new blog sources

--- push-and-publish.bat
+++ push-and-publish.bat
@@ -1,8 +1,3 @@
-
-REM git submodule foreach --recursive git add -A
-REM git submodule foreach --recursive git add -A
-REM git commit -m "new generated pages"
-
 cd themes/galdebert-hyde
 git add -A
 git commit -m "new template sources"
@@ -12,7 +7,6 @@ cd ../..
 git add -A
 git commit -m "new blog sources"
 git push
-
 
 hugo --cleanDestinationDir -d ../galdebert.github.io
 cd ../galdebert.github.io
